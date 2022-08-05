950912fef2cf05c36855f92d31bad0f698185605
KemiDoyin <kemidoy@gmail.com>
2022-08-05 09:49:08 +0100

    added yup validation

--- login-page/src/components/Login/login.js
+++ login-page/src/components/Login/login.js
@@ -1,5 +1,7 @@
-import { useForm } from 'react-hook-form';
-import swal from 'sweetalert';
+
+import {useFormik} from 'formik';
+import {FormControl,  FormErrorMessage, VStack, Input} from '@chakra-ui/react';
+import * as Yup from 'yup';
 import './login.css';
 import img1 from '../image/image 1.png';
 import user from '../image/Useric_user.png';
@@ -7,93 +9,76 @@ import lock from '../image/Lockic_lock.png';
 //import visible from '../image/Hideic_visible.png';
 
 function Login() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    mode: 'all',
-  });
-  const handleClick = () => {
-    let email = document.querySelector('#email');
-    let password = document.querySelector('#password');
-    let eMail = email.value;
-    let pWord = password.value;
-
-    if (eMail == '' || pWord == '') {
-      swal({
-        title: 'Error',
-        text: 'Please fill in the missing field',
-        icon: 'warning',
-        buttons: { cancel: true },
-      });
-    } else {
-      swal({
-        title: `Hi ${eMail}`,
-        text: 'your data has been successfully submitted',
-        icon: 'success',
-        button: 'Ok',
-      });
+ 
+  const pwdRule = /^(?=.*[0-9])(?=.*[!@#$%^&*.,])[a-zA-Z0-9!@#$%^&*.,]{6,16}$/;
+  const formik = useFormik({
+    initialValues: {email: '', password: ''},
+    validationSchema: Yup.object({
+      email: Yup.string()
+      .email('Please enter a valid email')
+      .required('Email required'),
+      password: Yup.string()
+      .min(8, 'Password too short')
+      .matches(pwdRule, {message: "Must contain at least one number, one uppercase and lowercase letter, a symbol and at least 8 or more characters"})
+      .required('Password required')
+    }),
+    onSubmit: (values, actions) => {
+      alert(JSON.stringify(values, null, 2))
+      actions.resetForm()
     }
-  };
+  })
 
-  return (
+  
+  return ( 
+    
     <div className="container">
       <div className="info" id="info">
         <h1>Log in to Teamwork</h1>
         <p>Forgot password?</p>
-
-        <form
+        <VStack as = "form">
+        <form 
+        onSubmit={formik.handleSubmit}
           className="validateForm"
-          onSubmit={handleSubmit((data) => console.log(data))}
         >
           <br />
+          <FormControl isInvalid= {formik.errors.email && formik.touched.email}>
           <label for="email">Email</label>
           <br />
           <img src={user} alt="user" className="user" />
-          <input
-            type="text"
+          <Input
+            type="email"
             name="email"
             placeholder="Email Address"
             className="email"
             id="email"
-            {...register('email', {
-              required: 'Email is required',
-              pattern: {
-                value:
-                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message: 'Looks like this is not an email',
-              },
-            })}
+            {...formik.getFieldProps('email')}
+            
           />
           <br />
-          <span className="error-block">{errors.email?.message}</span>
+          <span className='error-block'><FormErrorMessage>{formik.errors.email}</FormErrorMessage></span>
+          </FormControl>
           <br />
-          <label for="username">Password</label>
+          <FormControl isInvalid= {formik.errors.password && formik.touched.password}>
+          <label for="password">Password</label>
           <br />
 
           <img src={lock} alt="lock" className="lock" />
           {/* <img src={visible} alt="visible" className="visible" /> */}
-          <input
+          <Input
             type="password"
             name="password"
             placeholder=" insert Password"
             className="password"
             id="password"
-            {...register('password', {
-              required: 'Password cannot be empty',
-              pattern: {
-                value:
-                  /^(?=.*[0-9])(?=.*[!@#$%^&*.,])[a-zA-Z0-9!@#$%^&*.,]{6,16}$/,
-                message: `Must contain at least at least 8 or more characters`,
-              },
-            })}
+            {...formik.getFieldProps('password')}
+            
           />
-
           <br />
-          <span className="error-block">{errors.password?.message}</span>
+          <span className='error-block'><FormErrorMessage>{formik.errors.password}</FormErrorMessage></span>
+          </FormControl>
           <br />
         </form>
+        </VStack>
         {/* <div className="remember">
           <input
             type="checkbox"
@@ -105,7 +90,9 @@ function Login() {
           <p>Forgotten password?</p>
         </div> */}
         <div className="btn">
-          <button type="submit" id="btn" onClick={handleClick}>
+          <button type="submit" id="btn" 
+          onClick={formik.handleSubmit}
+          >
             Login
           </button>
         </div>
@@ -114,7 +101,11 @@ function Login() {
         <img src={img1} alt="img1" className="img1" />
       </div>
     </div>
+      
   );
+     
+      
 }
 
 export default Login;
+  
